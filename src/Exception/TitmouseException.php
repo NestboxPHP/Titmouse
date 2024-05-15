0c949e91fdb649d8f60aa465ddd9e850fa9b3490
supergnaw <loren.supernaw@gmail.com>
2024-05-14 20:09:51 -0500

    Update TitmouseException.php
    - namespace updates from refactor

--- src/Exception/TitmouseException.php
+++ src/Exception/TitmouseException.php
@@ -1,11 +1,16 @@
 <?php
 
-declare( strict_types = 1 );
+declare(strict_types = 1);
 
-namespace NestboxPHP\Nestbox\Titmouse;
+namespace NestboxPHP\Titmouse\Exception;
 
+use Exception;
 use NestboxPHP\Nestbox\Exception\NestboxException;
 
 class TitmouseException extends NestboxException
 {
+    public function __construct(string $message, int $code = 0, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
